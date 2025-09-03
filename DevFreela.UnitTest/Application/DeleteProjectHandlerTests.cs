e723addbcae43caeec6c9dc17da973ac4ed51d59
alysonmcechinel <alysonmcechinell@gmail.com>
2025-09-02 22:39:51 -0300

    add: test unit with moq

--- DevFreela.UnitTest/Application/DeleteProjectHandlerTests.cs
+++ DevFreela.UnitTest/Application/DeleteProjectHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Application.Commands.Project;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTest.Application;
@@ -48,5 +49,52 @@ public class DeleteProjectHandlerTests
 
         await repository.Received(1).GetById(Arg.Any<int>());
         await repository.DidNotReceive().Update(Arg.Any<Project>());
+    }
+
+    /*-------------------------------------------------------------------------------------------------*/
+
+    [Fact]
+    public async Task ProjectExists_Delete_Succes_Moq()
+    {
+        // Arrange
+        var project = new Project(1, 1, "Teste projeto", "Isso é uma descrição legal", 1000);
+
+        var repository = Mock.Of<IProjectRepository>(
+            x => x.GetById(It.IsAny<int>()) == Task.FromResult(project)
+            && x.Update(It.IsAny<Project>()) == Task.CompletedTask
+        );
+
+        var handler = new DeleteProjectHandler(repository);
+        var command = new DeleteProjectCommand(1);
+
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.True(result.IsSucess);
+        Mock.Get(repository).Verify(v => v.GetById(1), Times.Once);
+        Mock.Get(repository).Verify(v => v.Update(It.IsAny<Project>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task ProjectDoesNotExist_Delete_Error_Moq()
+    {
+        // Arrange
+        var repository = Mock.Of<IProjectRepository>(
+            x => x.GetById(It.IsAny<int>()) == Task.FromResult((Project?) null)
+        );
+
+        var handler = new DeleteProjectHandler(repository);
+        var command = new DeleteProjectCommand(1);
+
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.False(result.IsSucess);
+        Assert.Equal(DeleteProjectHandler.PROJECT_NOT_FOUND_MESSAGE, result.Message);
+
+        Mock.Get(repository).Verify(v => v.GetById(1), Times.Once);
+        Mock.Get(repository).Verify(v => v.Update(It.IsAny<Project>()), Times.Never);
     }
 }
--- DevFreela.UnitTest/Application/InsertProjectHandlerTests.cs
+++ DevFreela.UnitTest/Application/InsertProjectHandlerTests.cs
@@ -2,6 +2,7 @@
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
 using MediatR;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTest.Application;
@@ -37,5 +38,41 @@ public class InsertProjectHandlerTests
         await repository.Received(1).Add(Arg.Any<Project>());
     }
 
-    // TODO: implementar error
+    [Fact]
+    public async Task InputDataAreOK_Insert_Success_Moq()
+    {
+        // Arrange
+        const int ID = 1;
+
+        // mesma coisa que o repository pode ser feito das duas maneiras.
+        //var mock = new Mock<IProjectRepository>();
+        //mock.Setup(x => x.Add(It.IsAny<Project>())).ReturnsAsync(ID);
+        
+        var repository = Mock.Of<IProjectRepository>(x => x.Add(It.IsAny<Project>()) == Task.FromResult(ID));
+        var mediator = Mock.Of<IMediator>();
+
+        var command = new InsertProjectCommand
+        {
+            Title = "Projeto Teste",
+            Description = "Uma descrição legal do projeto",
+            TotalCost = 2500,
+            IdClient = 1,
+            IdFreelancer = 1
+        };
+
+        var handler = new InsertProjectHandler(repository, mediator);
+
+        // Act
+        var result = await handler.Handle(command, new CancellationToken());
+
+        // Assert
+        Assert.True(result.IsSucess);
+        Assert.Equal(ID, result.Data);
+
+        //mock.Verify(x => x.Add(It.IsAny<Project>()), Times.Once);
+
+        Mock.Get(repository).Verify(x => x.Add(It.IsAny<Project>()), Times.Once);
+    }
+
+    // TODO: implementar error com NSubstitute
 }
